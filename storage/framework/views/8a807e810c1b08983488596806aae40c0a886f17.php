e1d95f9def2033d907ee50c66f68d57eaead6521
Esca6585 <esca656585@gmail.com>
2021-10-09 09:46:15 +0500

    updated

--- storage/framework/views/8a807e810c1b08983488596806aae40c0a886f17.php
+++ storage/framework/views/8a807e810c1b08983488596806aae40c0a886f17.php
@@ -24,13 +24,14 @@
                 height="24px" viewBox="0 0 24 24" version="1.1">
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                     <rect x="0" y="0" width="24" height="24"></rect>
-                    <path d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 
-                        13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 
-                        L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" fill="#000000"
-                        fill-rule="nonzero"
-                        transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) ">
+                    <path
+                        d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z"
+                        fill="#000000" fill-rule="nonzero"
+                        transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953)">
                     </path>
-                    <rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"></rect>
+                    <path
+                        d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z"
+                        fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
                 </g>
             </svg>
         </span>
@@ -64,8 +65,8 @@
 
         <div class="example-preview">
             <!--begin::Modal-->
-            <div class="modal fade" id="exampleModalSizeSm-<?php echo e($category->id); ?>" tabindex="-1" aria-labelledby="exampleModalSizeSm-<?php echo e($category->id); ?>"
-                style="display: none;" aria-hidden="true">
+            <div class="modal fade" id="exampleModalSizeSm-<?php echo e($category->id); ?>" tabindex="-1"
+                aria-labelledby="exampleModalSizeSm-<?php echo e($category->id); ?>" style="display: none;" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
